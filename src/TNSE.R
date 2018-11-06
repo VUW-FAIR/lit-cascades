a95e4911cdbf8512e6aee98a94dad18c8942f118
Markus Luczak-Roesch <accounts@markus-luczak.de>
2018-11-07 10:08:47 +1300

    advanced node feature analysis

--- src/TNSE.R
+++ src/TNSE.R
@@ -78,6 +78,201 @@ tsne_plotting <- function(tsn_list, percent = .05){
     
   }
 }
+
+
+structuralFeatures <- function(nodes, links){
+  ## additional TIC features - testing
+  casc <- c()
+  inter <- c()
+  ent <- data.frame(ww = numeric(0), xx = numeric(0), yy = numeric(0), zz = numeric(0))
+  wien <- c()
+  colnames(links) <- c('source', 'target', 'tag')
+  colnames(nodes) <- c('id','title','label')
+  coordinates <- c()
+  spec <- list()
+  div = 1
+  
+  g1 <- igraph::make_empty_graph(n = 0, directed = TRUE)
+  struct <- c()
+  props <- c()
+  for(z in 1:nrow(nodes)){
+    if(nodes[z,]$title == ""){
+      if(nrow(ent) > 0){
+        ent <- rbind(ent, ent[nrow(ent),])
+        wien <- rbind(wien, wien[nrow(wien),])
+        
+      }else{
+        ent <- rbind(ent, c(0, 1, 0, 1))
+        wien <- rbind(wien, c(0, 1, 1))
+      }
+      coordinates <- rbind(coordinates, c(as.numeric(nodes[z, 1]), 0, 0))
+      
+    }else{
+      inter <- rbind(inter, paste(sort(unlist(strsplit(nodes[z,2],', '))), collapse = ', '))
+      nextI <- digest::digest(paste(sort(unlist(strsplit(nodes[z,2], ', '))), collapse = ', '), algo = "md5")
+      if(length(spec) == 0){
+        coordinates <- rbind(coordinates, c(as.numeric(nodes[z, 1]), 1, 1))
+        spec[[nextI]] <- c(1, 1)
+      }
+      else{
+        if(is.null(spec[[nextI]])){
+          spec[[nextI]] <- c(1,div)
+          coordinates <- rbind(coordinates,c(as.numeric(nodes[z,1]),spec[[nextI]][1],div))
+          div <- div+1
+        }else{
+          spec[[nextI]] <- c(spec[[nextI]][1]+1,spec[[nextI]][2])
+          coordinates <- rbind(coordinates,c(as.numeric(nodes[z,1]),spec[[nextI]][1],spec[[nextI]][2]))
+        }
+      }
+      
+      interact <- list()
+      cooccure <- list()
+      #temp1 <- c()
+      for(v in 1:nrow(inter)){
+        interactions <- unlist(strsplit(unlist(inter[v,1]),', '))
+        #temp1 <- rbind(temp1, gtools::combinations(length(interactions), 2, interactions))
+        for( m in 1:length(interactions)){
+          if(is.null(interact[[interactions[m]]])) interact[[interactions[m]]] <- 1
+          else interact[[interactions[m]]] <- interact[[interactions[m]]] + 1
+        }
+      }
+      df <- data.frame(unlist(interact))
+      tmp <- df[,1] / colSums(df)
+      df$loga <- log(tmp)
+      df$piloga <- tmp * log(tmp)
+      if(is.nan((-1 * (colSums(df)[3])) / log(nrow(df)))){
+        ent <- rbind(ent,c(entropy::entropy.empirical(df[,1], unit = "log2"), 1, -1 * (colSums(df)[3]), 1))
+      } else{
+        ent <- rbind(ent, c(entropy::entropy.empirical(df[,1], unit = "log2"), (-1 * (colSums(df)[3])) / log2(nrow(df)),-1*(colSums(df)[3]),(-1*(colSums(df)[3]))/log(nrow(df))))
+      }
+      
+      if(nrow(df) == 1){
+        wien <- rbind(wien, c(0, 1, 1))
+      } else{
+        H <- vegan::diversity(df[,1])
+        S <- nrow(df)
+        J <- H/log(S)
+        wien <- rbind(wien,c(H, J, S))
+      }}
+    
+    #}
+    colnames(ent)<-c('empEntropy', 'evenness_log2', 'entropy', 'evenness')
+    colnames(wien)<-c('ShannonWiener', 'Pielou', 'Richness')
+    
+    #add node
+    g1 <- igraph::add_vertices(g1,1,attr = list(id = as.numeric(nodes[z,1])))
+    
+    #add all links to node
+    theLinks <- unique(links[which(links[,2] == nodes[z,1]),1:2])
+    for(srclnk in theLinks[,1]){
+      g1 <- igraph::add_edges(g1, c(which(igraph::V(g1)$id == srclnk), which(igraph::V(g1)$id == as.numeric(nodes[z,1]))))
+    }
+    
+    #degd <- degree.distribution(g1)
+    wtc <- igraph::cluster_walktrap(g1)
+    struct <- rbind(struct, c(igraph::diameter(g1), igraph::edge_density(g1), igraph::modularity(wtc), igraph::cohesion(g1)))
+  }
+  
+  write.table(cbind(coordinates, wien, struct),
+              file=paste0("resources/output/", sliceSize, "/", theSource,
+                          "_temporal_statistics.csv"), row.names = F, col.names = F, sep = ";")
+  
+  nodeFeatures <- as.data.frame(cbind(nodes$title,wien,struct[,c(2,3)]),stringsAsFactors = F)
+  nodeFeatures$order <- rownames(nodeFeatures)
+  colnames(nodeFeatures) <- c("terms","entropy","evenness","richness","density","modularity","order")
+  nodeFeatures <- nodeFeatures[which(nodeFeatures$terms!=""),]
+  for(i in 1:nrow(nodeFeatures)){
+    if(length(unlist(strsplit(nodeFeatures$terms[i],", "))) > 1){
+      nextTerms <- unlist(strsplit(nodeFeatures$terms[i],", "))
+      nodeFeatures$terms[i] <- nextTerms[1]
+      print(i)
+      print(nextTerms[1])
+      for(j in 2:length(nextTerms)){
+        nodeFeatures <- rbind(nodeFeatures,data.frame(terms=nextTerms[j],entropy=nodeFeatures$entropy[i],evenness=nodeFeatures$evenness[i],richness=nodeFeatures$richness[i],density=nodeFeatures$density[i],modularity=nodeFeatures$modularity[i],order=nodeFeatures$order[i]))
+      }
+    }
+  }
+  nodeFeatures$order <- as.numeric(nodeFeatures$order)
+  nodeFeatures$entropy <- as.numeric(nodeFeatures$entropy)
+  nodeFeatures$evenness <- as.numeric(nodeFeatures$evenness)
+  nodeFeatures$richness <- as.numeric(nodeFeatures$richness)
+  nodeFeatures$density <- as.numeric(nodeFeatures$density)
+  nodeFeatures$modularity <- as.numeric(nodeFeatures$modularity)
+  nodeFeatures <- nodeFeatures[order(nodeFeatures$order),]
+  
+  termFeaturesNodes <- cbind(aggregate(entropy ~ terms,nodeFeatures,FUN = function(x){mean(diff(x))}),
+                             aggregate(evenness ~ terms,nodeFeatures,mean),
+                             aggregate(richness ~ terms,nodeFeatures,mean),
+                             aggregate(density ~ terms,nodeFeatures,mean),
+                             aggregate(modularity ~ terms,nodeFeatures,mean))
+  termFeaturesNodes$entropy[which(is.nan(termFeaturesNodes$entropy))] <- 0
+  termFeaturesNodes <- sort(termFeaturesNodes$terms)
+  
+  ktmp <- kmeans(termFeaturesNodes[-c(1,3,5,7,9)],5)
+  foo <- termFeaturesNodes[-c(1,3,5,7,9)]
+  plot(PCA, col=ktmp$cluster)
+  text(x=PCA[,1], y=PCA[,2], cex=0.6, pos=4, labels=(termFeaturesNodes$terms))
+  plot(PCA[,2],PCA[,3], col=ktmp$cluster)
+  text(x=PCA[,2], y=PCA[,3], cex=0.6, pos=4, labels=(termFeaturesNodes$terms))
+  plot(PCA[,1],PCA[,3], col=ktmp$cluster)
+  text(x=PCA[,1], y=PCA[,3], cex=0.6, pos=4, labels=(termFeaturesNodes$terms))
+  
+  # further analysis tests
+  
+  links$dist <- links$target-links$source
+  links$hlp <- 1
+  linkFeatures <- cbind(aggregate(dist ~ tag,links,mean),aggregate(hlp ~ tag,links,sum))
+  rownames(linkFeatures) <- linkFeatures$tag
+  
+  # what recurrs but what does not co-occur
+  recNOTco <- linkFeatures$tag[-which(linkFeatures$tag %in% rownames(cooc[[1]]))]
+  
+  # what co-occurs but doesn't recur
+  coNOTrec <- rownames(cooc[[1]])[-which(rownames(cooc[[1]]) %in% linkFeatures$tag)]
+  
+  #what does recur and co-occur
+  corec <- rownames(cooc[[1]])[which(rownames(cooc[[1]]) %in% linkFeatures$tag)]
+  
+  #what does occur but neither recur nor co-occur
+  occ <- termFeaturesNodes$terms[which(!termFeaturesNodes$terms %in% fusedTerms$terms)]
+  
+  #all terms
+  fusedTerms <- as.data.frame(sort(unique(c(recNOTco,coNOTrec,corec,occ))),stringsAsFactors = F)
+  colnames(fusedTerms) <- c("terms")
+  rownames(fusedTerms) <- fusedTerms$terms
+  fusedTerms$coocs <- 0
+  fusedTerms$distcoocs <- 0
+  fusedTerms$recs <- 0
+  fusedTerms$distrecs <- 0
+  
+  #co-occ featrues
+  fusedTerms[coNOTrec,2] <- colSums(cooc[[1]][coNOTrec])
+  fusedTerms[corec,2] <- colSums(cooc[[1]][corec])
+  
+  fusedTerms[coNOTrec,3] <- colSums(cooc[[1]][coNOTrec] > 0)
+  fusedTerms[corec,3] <- colSums(cooc[[1]][corec] > 0)
+  
+  fusedTerms$coocProp <- 0
+  fusedTerms[which(fusedTerms$coocs>0),6] <- fusedTerms[which(fusedTerms$coocs>0),3] / fusedTerms[which(fusedTerms$coocs>0),2]
+  
+  fusedTerms[corec,4] <- linkFeatures[corec,4]
+  fusedTerms[recNOTco,4] <- linkFeatures[recNOTco,4]
+  
+  fusedTerms[corec,5] <- linkFeatures[corec,2]
+  fusedTerms[recNOTco,5] <- linkFeatures[recNOTco,2]
+  
+  structuralFeatures <- cbind(fusedTerms,termFeaturesNodes)
+  
+  ktmp <- kmeans(structuralFeatures[-c(1,2,3,7,9,11,13,15)],5)
+  foo <- structuralFeatures[-c(1,2,3,7,9,11,13,15)]
+  PCA <-prcomp(foo)$x
+  plot(PCA, col=ktmp$cluster)
+  text(x=PCA[,1], y=PCA[,2], cex=0.6, pos=4, labels=(row.names(foo)))
+  plot(PCA[,2],PCA[,3], col=ktmp$cluster)
+  text(x=PCA[,2], y=PCA[,3], cex=0.6, pos=4, labels=(row.names(foo)))
+  plot(PCA[,1],PCA[,3], col=ktmp$cluster)
+  text(x=PCA[,1], y=PCA[,3], cex=0.6, pos=4, labels=(row.names(foo)))
+}
 # Start of the Code -------------------------------------------------------
 
 
@@ -136,6 +331,11 @@ for(sent in cooc){
 
 tsne_plotting(tsn_list)
 
+# structural feature analysis
+for(nextBook in 1:length(link)){
+  structuralFeatures(node[[nextBook]],link[[nextBook]])
+}
+
 #random forest example
 
 rf.fit <- randomForest::randomForest(x = cooc[[1]], y = NULL, ntree = 10000, proximity = TRUE, oob.prox = TRUE)
@@ -153,200 +353,6 @@ plot(fit) # plot results
 summary(fit) # display the best model
 #evaluate cluster solutions
 #cluster.stats(cooc[[1]], fit1$cluster, fit2$cluster)
-
-
-## additional TIC features - testing
-casc <- c()
-inter <- c()
-ent <- data.frame(ww = numeric(0), xx = numeric(0), yy = numeric(0), zz = numeric(0))
-wien <- c()
-colnames(link[[1]]) <- c('source', 'target', 'tag')
-colnames(node[[1]]) <- c('id','title','label')
-coordinates <- c()
-spec <- list()
-div = 1
-
-g1 <- igraph::make_empty_graph(n = 0, directed = TRUE)
-struct <- c()
-props <- c()
-for(z in 1:nrow(node[[1]])){
-  if(node[[1]][z,]$title == ""){
-    if(nrow(ent) > 0){
-      ent <- rbind(ent, ent[nrow(ent),])
-      wien <- rbind(wien, wien[nrow(wien),])
-      
-    }else{
-      ent <- rbind(ent, c(0, 1, 0, 1))
-      wien <- rbind(wien, c(0, 1, 1))
-    }
-    coordinates <- rbind(coordinates, c(as.numeric(node[[1]][z, 1]), 0, 0))
-    
-  }else{
-    inter <- rbind(inter, paste(sort(unlist(strsplit(node[[1]][z,2],', '))), collapse = ', '))
-    nextI <- digest::digest(paste(sort(unlist(strsplit(node[[1]][z,2], ', '))), collapse = ', '), algo = "md5")
-    if(length(spec) == 0){
-      coordinates <- rbind(coordinates, c(as.numeric(node[[1]][z, 1]), 1, 1))
-      spec[[nextI]] <- c(1, 1)
-    }
-    else{
-      if(is.null(spec[[nextI]])){
-        spec[[nextI]] <- c(1,div)
-        coordinates <- rbind(coordinates,c(as.numeric(node[[1]][z,1]),spec[[nextI]][1],div))
-        div <- div+1
-      }else{
-        spec[[nextI]] <- c(spec[[nextI]][1]+1,spec[[nextI]][2])
-        coordinates <- rbind(coordinates,c(as.numeric(node[[1]][z,1]),spec[[nextI]][1],spec[[nextI]][2]))
-      }
-    }
-    
-    interact <- list()
-    cooccure <- list()
-    #temp1 <- c()
-    for(v in 1:nrow(inter)){
-      interactions <- unlist(strsplit(unlist(inter[v,1]),', '))
-      #temp1 <- rbind(temp1, gtools::combinations(length(interactions), 2, interactions))
-      for( m in 1:length(interactions)){
-        if(is.null(interact[[interactions[m]]])) interact[[interactions[m]]] <- 1
-        else interact[[interactions[m]]] <- interact[[interactions[m]]] + 1
-      }
-    }
-    df <- data.frame(unlist(interact))
-    tmp <- df[,1] / colSums(df)
-    df$loga <- log(tmp)
-    df$piloga <- tmp * log(tmp)
-    if(is.nan((-1 * (colSums(df)[3])) / log(nrow(df)))){
-      ent <- rbind(ent,c(entropy::entropy.empirical(df[,1], unit = "log2"), 1, -1 * (colSums(df)[3]), 1))
-    } else{
-      ent <- rbind(ent, c(entropy::entropy.empirical(df[,1], unit = "log2"), (-1 * (colSums(df)[3])) / log2(nrow(df)),-1*(colSums(df)[3]),(-1*(colSums(df)[3]))/log(nrow(df))))
-    }
-    
-    if(nrow(df) == 1){
-      wien <- rbind(wien, c(0, 1, 1))
-    } else{
-      H <- vegan::diversity(df[,1])
-      S <- nrow(df)
-      J <- H/log(S)
-      wien <- rbind(wien,c(H, J, S))
-    }}
-  
-  #}
-  colnames(ent)<-c('empEntropy', 'evenness_log2', 'entropy', 'evenness')
-  colnames(wien)<-c('ShannonWiener', 'Pielou', 'Richness')
-  
-  #add node
-  g1 <- igraph::add_vertices(g1,1,attr = list(id = as.numeric(node[[1]][z,1])))
-  
-  #add all links to node
-  theLinks <- unique(link[[1]][which(link[[1]][,2] == node[[1]][z,1]),1:2])
-  for(srclnk in theLinks[,1]){
-    g1 <- igraph::add_edges(g1, c(which(igraph::V(g1)$id == srclnk), which(igraph::V(g1)$id == as.numeric(node[[1]][z,1]))))
-  }
-  
-  #degd <- degree.distribution(g1)
-  wtc <- igraph::cluster_walktrap(g1)
-  struct <- rbind(struct, c(igraph::diameter(g1), igraph::edge_density(g1), igraph::modularity(wtc), igraph::cohesion(g1)))
-}
-
-write.table(cbind(coordinates, wien, struct),
-            file=paste0("resources/output/", sliceSize, "/", theSource,
-                        "_temporal_statistics.csv"), row.names = F, col.names = F, sep = ";")
-
-nodeFeatures <- as.data.frame(cbind(node[[1]]$title,wien,struct[,c(2,3)]),stringsAsFactors = F)
-nodeFeatures$order <- rownames(nodeFeatures)
-colnames(nodeFeatures) <- c("terms","entropy","evenness","richness","density","modularity","order")
-nodeFeatures <- nodeFeatures[which(nodeFeatures$terms!=""),]
-for(i in 1:nrow(nodeFeatures)){
-  if(length(unlist(strsplit(nodeFeatures$terms[i],", "))) > 1){
-    nextTerms <- unlist(strsplit(nodeFeatures$terms[i],", "))
-    nodeFeatures$terms[i] <- nextTerms[1]
-    print(i)
-    print(nextTerms[1])
-    for(j in 2:length(nextTerms)){
-      nodeFeatures <- rbind(nodeFeatures,data.frame(terms=nextTerms[j],entropy=nodeFeatures$entropy[i],evenness=nodeFeatures$evenness[i],richness=nodeFeatures$richness[i],density=nodeFeatures$density[i],modularity=nodeFeatures$modularity[i],order=nodeFeatures$order[i]))
-    }
-  }
-}
-nodeFeatures$order <- as.numeric(nodeFeatures$order)
-nodeFeatures$entropy <- as.numeric(nodeFeatures$entropy)
-nodeFeatures$evenness <- as.numeric(nodeFeatures$evenness)
-nodeFeatures$richness <- as.numeric(nodeFeatures$richness)
-nodeFeatures$density <- as.numeric(nodeFeatures$density)
-nodeFeatures$modularity <- as.numeric(nodeFeatures$modularity)
-nodeFeatures <- nodeFeatures[order(nodeFeatures$order),]
-
-termFeaturesNodes <- cbind(aggregate(entropy ~ terms,nodeFeatures,FUN = function(x){mean(diff(x))}),
-                           aggregate(evenness ~ terms,nodeFeatures,mean),
-                           aggregate(richness ~ terms,nodeFeatures,mean),
-                           aggregate(density ~ terms,nodeFeatures,mean),
-                           aggregate(modularity ~ terms,nodeFeatures,mean))
-termFeaturesNodes$entropy[which(is.nan(termFeaturesNodes$entropy))] <- 0
-termFeaturesNodes <- sort(termFeaturesNodes$terms)
-
-ktmp <- kmeans(termFeaturesNodes[-c(1,3,5,7,9)],5)
-foo <- termFeaturesNodes[-c(1,3,5,7,9)]
-plot(PCA, col=ktmp$cluster)
-text(x=PCA[,1], y=PCA[,2], cex=0.6, pos=4, labels=(termFeaturesNodes$terms))
-plot(PCA[,2],PCA[,3], col=ktmp$cluster)
-text(x=PCA[,2], y=PCA[,3], cex=0.6, pos=4, labels=(termFeaturesNodes$terms))
-plot(PCA[,1],PCA[,3], col=ktmp$cluster)
-text(x=PCA[,1], y=PCA[,3], cex=0.6, pos=4, labels=(termFeaturesNodes$terms))
-
-# further analysis tests
-
-link[[1]]$dist <- link[[1]]$target-link[[1]]$source
-link[[1]]$hlp <- 1
-linkFeatures <- cbind(aggregate(dist ~ tag,link[[1]],mean),aggregate(hlp ~ tag,link[[1]],sum))
-rownames(linkFeatures) <- linkFeatures$tag
-
-# what recurrs but what does not co-occur
-recNOTco <- linkFeatures$tag[-which(linkFeatures$tag %in% rownames(cooc[[1]]))]
-
-# what co-occurs but doesn't recur
-coNOTrec <- rownames(cooc[[1]])[-which(rownames(cooc[[1]]) %in% linkFeatures$tag)]
-
-#what does recur and co-occur
-corec <- rownames(cooc[[1]])[which(rownames(cooc[[1]]) %in% linkFeatures$tag)]
-
-#what does occur but neither recur nor co-occur
-occ <- termFeaturesNodes$terms[which(!termFeaturesNodes$terms %in% fusedTerms$terms)]
-
-#all terms
-fusedTerms <- as.data.frame(sort(unique(c(recNOTco,coNOTrec,corec,occ))),stringsAsFactors = F)
-colnames(fusedTerms) <- c("terms")
-rownames(fusedTerms) <- fusedTerms$terms
-fusedTerms$coocs <- 0
-fusedTerms$distcoocs <- 0
-fusedTerms$recs <- 0
-fusedTerms$distrecs <- 0
-
-#co-occ featrues
-fusedTerms[coNOTrec,2] <- colSums(cooc[[1]][coNOTrec])
-fusedTerms[corec,2] <- colSums(cooc[[1]][corec])
-
-fusedTerms[coNOTrec,3] <- colSums(cooc[[1]][coNOTrec] > 0)
-fusedTerms[corec,3] <- colSums(cooc[[1]][corec] > 0)
-
-fusedTerms$coocProp <- 0
-fusedTerms[which(fusedTerms$coocs>0),6] <- fusedTerms[which(fusedTerms$coocs>0),3] / fusedTerms[which(fusedTerms$coocs>0),2]
-
-fusedTerms[corec,4] <- linkFeatures[corec,4]
-fusedTerms[recNOTco,4] <- linkFeatures[recNOTco,4]
-
-fusedTerms[corec,5] <- linkFeatures[corec,2]
-fusedTerms[recNOTco,5] <- linkFeatures[recNOTco,2]
-
-structuralFeatures <- cbind(fusedTerms,termFeaturesNodes)
-
-ktmp <- kmeans(structuralFeatures[-c(1,2,3,7,9,11,13,15)],5)
-foo <- structuralFeatures[-c(1,2,3,7,9,11,13,15)]
-PCA <-prcomp(foo)$x
-plot(PCA, col=ktmp$cluster)
-text(x=PCA[,1], y=PCA[,2], cex=0.6, pos=4, labels=(row.names(foo)))
-plot(PCA[,2],PCA[,3], col=ktmp$cluster)
-text(x=PCA[,2], y=PCA[,3], cex=0.6, pos=4, labels=(row.names(foo)))
-plot(PCA[,1],PCA[,3], col=ktmp$cluster)
-text(x=PCA[,1], y=PCA[,3], cex=0.6, pos=4, labels=(row.names(foo)))
-
 
 # testing some recurrence network analysis
 g <- igraph::graph_from_data_frame(link[[1]])
