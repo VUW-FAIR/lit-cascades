3022c95c3cd20bb95779b4b19647aee3605941dd
d1v1nagrace <d1v1nagrace@yahoo.com>
2019-02-13 10:27:49 +1300

    update

--- Data Visualisations/Circular_Plots.R
+++ Data Visualisations/Circular_Plots.R
@@ -2,16 +2,16 @@
 ###Updated Circular Graphs
 
 
-#set directory
+#SET DIRECTORY
 DIRECTORY<-"pda1710-sentence-advs-lemma-book-centric" 
 
 
-#prep the matrices
+#PREP THE MATRICES
 setwd("/home/STAFF/cabautgr/projects/tic-personality-words/")
 
 getwd()
 setwd("..")
-## Creating a list with all co-ocurrence matrices for all outputs
+
 ## Creating a list with all co-ocurrence matrices for all outputs
 allmatrices <- list.files(paste0("outputs save/",DIRECTORY),
                           pattern = "(.*)_network_matrix.csv",
@@ -39,10 +39,10 @@ for(bb in 1:length(cooc)){
 }
 
 
-#duplicate of circ plot examples.r
-#creating circular graphs:
+#CIRCULAR GRAPHS
 
 library(circlize)
+library(RColorBrewer)
 
 #Setting directory
 setwd("outputs save")
@@ -54,19 +54,18 @@ setwd("..") #if you would like to go back one directory
 clusterscsv <- list(dir(pattern = "*-TSNE-cluster.csv")) 
 
 ##do a loop here
-#for(k in 3:length(cooc)){
-k<-3
+#k<-3
+for(k in 1:length(cooc)){
+
 mat<-as.matrix(cooc[[k]])
 mat[lower.tri(mat)] <- 0
 par(mar = c(0, 0, 0, 0))
-
 
 cfile<-clusterscsv[[1]][k] 
 
 #get the title and position k
 clusters<-read.csv(file=cfile,header=T,sep=";")
 
-library(RColorBrewer)
 n <- nrow(unique(clusters))
 
 qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
@@ -81,43 +80,22 @@ grid.col <- setNames(colframe$colcode, colframe$terms)
 
 Title<-gsub("\\_", " ",sub(pattern = "-TSNE-cluster(.*)\\..*$", replacement = "\\1", basename(cfile)))
 
+#circular graph
 #title(main=Title,col.main="gray",adj=0.15,line=-2)
-makeCircos<-function(){
-  circos.par(gap.degree = 0)
   chordDiagram(mat, directional = FALSE, annotationTrack = "grid", preAllocateTracks = 1, grid.col = grid.col)
-  #plot.new()
   circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
     xlim = get.cell.meta.data("xlim")
     ylim = get.cell.meta.data("ylim")
     sector.name = get.cell.meta.data("sector.index")
     circos.text(mean(xlim), ylim[1] + .1, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
-    #circos.axis(h = "top", labels.cex = 0.5, major.tick.percentage = 0.2, sector.index = sector.name, track.index = 2)
   }, bg.border = NA)
-}
-
-makeCircos()
 
 #save the graph in the PostProc folder of the directory
 BookTitle<-sub(pattern = "-TSNE-cluster(.*)\\..*$", replacement = "\\1", basename(cfile))
 dev.copy2pdf(file = paste0(paste0("../",DIRECTORY,"/","PostProc","/"),BookTitle,k,"_","clusterCircPlot.pdf"), height=10, width=10,out.type = "pdf",onefile=FALSE)
 dev.off()
 circos.clear()
-#gc()
-#}
-
-# rm(mat)
-# rm(cfile)
-# rm(clusters)
-# rm(n)
-# rm(qual_col_pals)
-# rm(col_vector)
-# rm(colcodes)
-# rm(colframe)
-# rm(colframe$colcode)
-# rm(grid.col)
-# gc()
-# rm(allmatrices)
-# gc()
+}
 
 ###-------------------------------------
 
