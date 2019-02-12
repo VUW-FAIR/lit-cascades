5ab4014b353b8a153035c5728a959aa1e77a9e9d
d1v1nagrace <d1v1nagrace@yahoo.com>
2019-02-13 10:48:35 +1300

    words_not_present update for pda lists

--- Data Visualisations/Words_Not_Present.R
+++ Data Visualisations/Words_Not_Present.R
@@ -3,56 +3,48 @@
 
 #Setting directory
 begPath<-"/Users/GraceC/TIC-VUW" #<- example
-setwd(paste0(begPath,"tic-personality-words/outputs save"))
+setwd(paste0("tic-personality-words/outputs save"))
 setwd("TIC-VUW/tic-personality-words/outputs save")
-DIRECTORY<-"pda500-sentence-advs-lemma-book-centric" #**
+DIRECTORY<-"pda1710-sentence-advs-lemma-book-centric" #**
 setwd(DIRECTORY)
 getwd()
 setwd("..") #if you would like to go back one directory
+setwd("PostProc")
+
 
 #all the nodes.csv files in the directory
-nodescsv <- list(dir(pattern = "*nodes.csv")) 
+nodescsv <- list(dir(pattern = "*nodes.csv"))
 
 #Note: TXTFILE needs to be in the same directory as DIRECTORY
-TXTFILE<-"pda500.txt" #**
+TXTFILE<-"pda1710.txt" #**
 
 #Set word list to compare with:
-wordlist<-read.csv(file=TXTFILE, header=T, sep="\t") 
+wordlist<-read.csv(file=TXTFILE, header=T, sep="\t")
 wordlist<-tolower(as.vector(wordlist[,1]))
 wordlist<-unique(wordlist)
 
-#Loop that goes through all files:
-for (k in 1:length(nodescsv[[1]])){
-  file<- nodescsv[[1]][k]
-  csvfile<-FrequencyWordsNotPresent(file,wordlist)
-  write.csv(csvfile, file = paste0(paste0("../",DIRECTORY,"/","PostProc","/"),file,k,"__WordsNotPresent",".txt")) 
-}
-
 #Function that returns a list of the words not present:
 FrequencyWordsNotPresent<-function(FileToRead,WordList){
-  
-  BookData <-read.csv(file=FileToRead, header=T, sep="") #Read CSV into R as a dataframe
-  words <- as.vector(BookData[,2]) #vector type
-  allwords<-unlist(strsplit(words,split=", ",fixed = FALSE)) #splits the two word values
-  allwords<-sort(allwords) #alphabetical order
-  uniqueWords<-sort(unique(allwords)) #removes duplicates and shows in alphabetical order
-  
-  wordsNotPresent<-wordlist[which(!wordlist %in% uniqueWords)]
-  
-  return(wordsNotPresent)
-  
+    
+    BookData <-read.csv(file=FileToRead, header=T, sep="") #Read CSV into R as a dataframe
+    words <- as.vector(BookData[,2]) #vector type
+    allwords<-unlist(strsplit(words,split=", ",fixed = FALSE)) #splits the two word values
+    allwords<-sort(allwords) #alphabetical order
+    uniqueWords<-sort(unique(allwords)) #removes duplicates and shows in alphabetical order
+    
+    wordsNotPresent<-wordlist[which(!wordlist %in% uniqueWords)]
+    
+    return(wordsNotPresent)
+    
+}
+
+#Loop that goes through all files:
+for (k in 1:length(nodescsv[[1]])){
+    file<- nodescsv[[1]][k]
+    csvfile<-FrequencyWordsNotPresent(file,wordlist)
+    write.csv(csvfile, file = paste0(paste0("../",DIRECTORY,"/","PostProc","/"),file,k,"__WordsNotPresent",".txt"))
 }
 
 
-##For testing purposes:
-test<-FrequencyWordsNotPresent(nodescsv[[1]][1],wordlist)
-getwd()
-test
-
-BookData <-read.csv(file=nodescsv[[1]][1], header=T, sep="") #Read CSV into R as a dataframe
-words <- as.vector(BookData[,2]) #vector type
-allwords<-unlist(strsplit(words,split=", ",fixed = FALSE)) #splits the two word values
-allwords<-sort(allwords) #alphabetical order
-uniqueWords<-sort(unique(allwords)) #removes duplicates and shows in alphabetical order
-
+######-----------------------------------------------
 
